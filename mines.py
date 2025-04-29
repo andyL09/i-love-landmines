3faa46803e0742a53bb35d2cc4a0756a2ca7d53e
Trevor Jordan <tmjordan5@cps.edu>
2025-04-29 10:19:28 -0500

    there is no reason for this not to work

--- mines.py
+++ mines.py
@@ -4,14 +4,18 @@ from sklearn.model_selection import train_test_split
 
 print("glug")
 
-# data = pd.read_csv("Mine_Dataset.csv")
-# x = data[["V","H","S"]].values
-# y = data["M"].values
+data = pd.read_csv("Mine-Data.csv")
+x = data[["V","H","S"]].values
+y = data["M"].values
 
-#xtrain,xtest,ytrain,ytest=train_test_split(x,y,test_size=.2)
+#train,test=train_test_split(real_data,test_size=.2)
+# placeholder_data=[([1, 1], [0]), ([1, 0], [1]), ([0, 1], [1]), ([0, 0], [0])]
 
-placeholder_data=[([1, 1], [0]), ([1, 0], [1]), ([0, 1], [1]), ([0, 0], [0])]
+tupleset=[]
+for i in range(len(y)):
+    tupleset.append((x[i],y[i]))
+    print(x[i])
 
-net = NeuralNet(2, 3, 1)
-net.train(placeholder_data)
-print(net.test_with_expected(placeholder_data))
+net = NeuralNet(3, 8, 1)
+net.train(tupleset)
+print(net.test_with_expected(tupleset))
